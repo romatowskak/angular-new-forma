30a28ab3d5be255b09df830ba5567ebce2aa244a
romatowskak <romatowskak@gmail.com>
2019-11-07 11:37:44 +0100

    "layout improvement"

--- src/app/tasks.service.ts
+++ src/app/tasks.service.ts
@@ -3,10 +3,11 @@ import { Observable, from } from 'rxjs';
 
 export interface ActionTasksElement {
   title: string;
+  projectName: string;
   type: string;
   completed: string;
   dueDate: string;
-  daysLeft: number;
+  daysLeft: string;
 }
 
 @Injectable({
@@ -17,52 +18,58 @@ export class TasksService {
   tableData: ActionTasksElement[] = [
     {
       title: 'Android - UI Automation Test',
+      projectName: 'CASD Wilson & Lamberton Middle Schools',
       type: 'General',
       completed: '80%',
       dueDate: '23/12/2019',
-      daysLeft: 21
+      daysLeft: 'TODO: count amount'
     },
     {
       title: 'The Flash Tutorial',
+      projectName: 'CASD Wilson & Lamberton Middle Schools',
       type: 'General',
       completed: '70%',
       dueDate: '26/12/2019',
-      daysLeft: 26
+      daysLeft: 'TODO: count amount'
     },
     {
       title: 'Cleaning and Organising Your Computer',
+      projectName: 'CASD Wilson & Lamberton Middle Schools',
       type: 'Clash',
       completed: '0%',
       dueDate: '23/12/2019',
-      daysLeft: 27
+      daysLeft: 'TODO: count amount'
     },
     {
       title: 'Android - UI Automation Test',
+      projectName: 'CASD Wilson & Lamberton Middle Schools',
       type: 'General',
       completed: '80%',
       dueDate: '23/12/2019',
-      daysLeft: 21
+      daysLeft: 'TODO: count amount'
     },
     {
       title: 'The Flash Tutorial',
+      projectName: 'CASD Wilson & Lamberton Middle Schools',
       type: 'General',
       completed: '70%',
       dueDate: '26/12/2019',
-      daysLeft: 26
+      daysLeft: 'TODO: count amount'
     },
     {
       title: 'Cleaning and Organising Your Computer',
+      projectName: 'CASD Wilson & Lamberton Middle Schools',
       type: 'Clash',
       completed: '0%',
       dueDate: '23/12/2019',
-      daysLeft: 27
+      daysLeft: 'TODO: count amount'
     }
   ];
   getAllTasks() {
     return new Observable(observer => {
       setInterval(() => {
         observer.next(this.tableData);
-      }, 2000);
+      }, 1000);
     });
   }
 }
