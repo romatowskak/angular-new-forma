64b27033a8412233d2e1d562028e6745842ad999
romatowskak <romatowskak@gmail.com>
2019-11-15 15:20:54 +0100

    "unit testing the circle-color pipe"

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -24,11 +24,4 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('examp-ng-app');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('examp-ng-app app is running!');
-  });
 });
