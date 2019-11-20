09c9ca1b8944a5adad3d8d013be62f2f077fd8b8
romatowskak <romatowskak@gmail.com>
2019-11-20 15:20:29 +0100

    "test for daysLeftToDeadline.component"

--- src/app/services/daysLeftToDeadlineService/days-left-to-deadline.service.spec.ts
+++ src/app/services/daysLeftToDeadlineService/days-left-to-deadline.service.spec.ts
@@ -10,4 +10,9 @@ describe('DaysLeftToDeadlineService', () => {
   it('should be created', () => {
     expect(daysLeftToDeadlineService).toBeTruthy();
   });
+
+  it('should calculate difference between dates', () => {
+    const diff = daysLeftToDeadlineService.daysLeftToDeadline(new Date('2019/12/29'), new Date('2019/12/28'));
+    expect(diff).toBe(1);
+  });
 });
