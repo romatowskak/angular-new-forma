01b3a1d0b260374b571eb856dc0fbc78e39a30bb
odudycz <odudycz@newforma.com>
2019-11-29 09:16:01 +0100

    Update days-left-to-deadline.service.ts

--- src/app/services/daysLeftToDeadlineService/days-left-to-deadline.service.ts
+++ src/app/services/daysLeftToDeadlineService/days-left-to-deadline.service.ts
@@ -7,10 +7,9 @@ export class DaysLeftToDeadlineService {
   daysLeftToDeadline(dueDate: Date, currentDate: Date) {
     if (typeof dueDate === 'string') {
       return;
-    } else {
-      const diffBetweenDates = dueDate.getTime() - currentDate.getTime();
-      const daysDifference = Math.round(diffBetweenDates / (1000 * 3600 * 24));
-      return daysDifference;
-    }
+    } 
+    const diffBetweenDates = dueDate.getTime() - currentDate.getTime();
+    const daysDifference = Math.round(diffBetweenDates / (1000 * 3600 * 24));
+    return daysDifference;
   }
 }
